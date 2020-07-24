66d002690d890a6667ccdc0e98df90d45aa668ac
furno west <furnowest57@gmail.com>
2020-07-24 10:25:17 -0700

    app.js routers fix

--- african-market/src/App.js
+++ african-market/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import Nav from "./components/Nav"
+import Signup from "./components/Signup"
+import Login from "./components/Login"
+import Home from "./components/Home"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Welcome to African Marketplace</h1>
+    <div>
+      <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
