a2c9aa9af8414328e710a30c877c964261734390
furno west <furnowest57@gmail.com>
2020-07-27 14:20:52 -0700

    added some more css

--- african-market/src/components/Login.js
+++ african-market/src/components/Login.js
@@ -1,11 +1,106 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Nav from "./Nav"
+import * as yup from "yup";
+import axios from "axios";
 
-function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+
+const formSchema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
+
+export default function LoginForm() {
+  const [formState, setFormState] = useState({
+    Password: "",
+    Username: ""
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  const [errorState, setErrorState] = useState({
+    password: "",
+    username: ""
+  });
+
+  const validate = e => {
+    let value =
+      e.target.value;
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(value)
+      .then(valid => {
+        setErrorState({
+          ...errorState,
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        setErrorState({
+          ...errorState,
+          [e.target.name]: err.errors[0]
+        });
+      });
+  };
+
+  const inputChange = e => {
+    e.persist();
+    validate(e);
+    let value =
+      e.target.value;
+    setFormState({ ...formState, [e.target.name]: value });
+  };
+
+  const formSubmit = e => {
+    e.preventDefault();
+    console.log("form submitted!");
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  };
+  return (
+    <div>
+      <Nav />
+
+      <form onSubmit={formSubmit}>
+
+        <h2>Welcome</h2>
+        <label htmlFor="usernameInput">
+          Username
+
+          <input
+            id="usernameInput"
+            type="username"
+            name="username"
+
+            value={formState.username}
+            onChange={inputChange}
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          Password
+
+          <input id="passwordInput"
+            type="password"
+            name="password"
+
+            value={formState.password}
+            onChange={inputChange}
+          />
+        </label>
+
+        <button disabled={buttonDisabled}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+
+
